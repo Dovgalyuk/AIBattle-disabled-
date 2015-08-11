1bbfe9bc9740d64321cfaf92347b61466894256b
Pavel Dovgaluk <pavel.dovgaluk@gmail.com>
2015-08-11 18:07:32 +0300

    system pause hack

--- site/cl.bat
+++ site/cl.bat
@@ -1,5 +1,5 @@
 call "%VS100COMNTOOLS%vsvars32.bat"
-cl.exe /MD /TP /Ox .\executions\%1 /Fe.\executions_bin\ /Fo.\executions_bin\
+cl.exe /MD /TP /Ox /Dsystem=aibattle_hack .\executions\%1 /Fe.\executions_bin\ /Fo.\executions_bin\
 SET TMP=%ERRORLEVEL%
 del .\executions_bin\*.obj
 exit %TMP%
