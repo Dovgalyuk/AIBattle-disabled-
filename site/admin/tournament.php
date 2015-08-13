677c4c0783425e1920283f3b2b160fb64ff3523c
Pavel Dovgaluk <pavel.dovgaluk@gmail.com>
2015-08-13 20:26:39 +0300

    Update admin panel

--- site/admin/tournament.php
+++ site/admin/tournament.php
@@ -1,7 +1,3 @@
-<script>
-    changeActiveAdminButton('tournamentsButton');
-</script>
-
 <?php
     $id = intval($_GET["id"]);
     if ($id == -1)
@@ -10,6 +6,7 @@
         $name = "";
         $description = "";
         $state = "preparing";
+        $checker = -1;
     }
     else
     {
@@ -18,8 +15,52 @@
         $name = $tour['name'];
         $description = trim($tour['description']);
         $state = $tour['state'];
+        $checker = $tour['defaultChecker'];
     }
 ?>
+<script>
+    changeActiveAdminButton('tournamentsButton');
+    
+    function submitForm()
+    {
+        var name = document.getElementById('name').value;
+        var description = CKEDITOR.instances.description.getData();
+        var state = document.getElementById('state').value;
+        var checker = document.getElementById('checker').value;
+        var id = <?php echo $id; ?>;
+        var game = <?php echo $gameId; ?>;
+        
+        if (name != '')
+        {
+            var data = {'tournamentSelector' : id,
+                        'tournamentName' : name,
+                        'gameSelector' : game,
+                        'tournamentDescription' : description,
+                        'tournamentState' : state,
+                        'defaultChecker' : checker};
+            
+            if (id == -1)
+                data['createTournament'] = true;
+            else
+                data['updateTournament'] = true;
+            
+            $.post
+            (   "jqueryTournament.php", 
+                data,
+                function (data)
+                {
+                    showModalAlert(data);
+                    window.location.search = "?page=tournaments";
+                }
+            );
+        }
+        else
+        {
+            showModalAlert('Название турнира не должно быть пустым!');
+        }
+    }
+</script>
+
 <form role="form" method="post">
     <div class="form-group">
         <label for="name" class = "APfont">Название турнира:</label>
@@ -38,6 +79,34 @@
     </div>
     <br>
     <div class="form-group">
+        <label for = "checker" class = "APfont">Тестировщик по умолчанию:</label>
+        <select id = "checker" class="form-control">
+        </select>
+    </div>
+    <script>
+        var gameId = <?php echo $gameId; ?>;
+        var p = <?php echo $checker; ?>;
+        
+        $.post
+        (
+            'jqueryGetCheckerList.php',
+            {'gameId' : gameId},
+            function (data)
+            {
+                var selectData = JSON.parse(data);
+                var list = document.getElementById('checker');
+                
+                $("#checker").empty();
+                
+                for (var i = 0; i < selectData.length; ++i)
+                {
+                    list.options[i] = new Option(selectData[i].name, selectData[i].id, false, p == selectData[i].id);
+                }
+            }
+        );
+    </script>            
+    <br>
+    <div class="form-group">
         <label for="state" class = "APfont">Статус турнира:</label>
         <select id = "state" class="form-control">
             <option value = "preparing">Подготовка к открытию</option>
@@ -48,5 +117,16 @@
             setSelectedIndexInSelector('state', '<?php echo $state; ?>');
         </script>
     </div>
-    
+    <div class="btn-group">
+        <button type = "submit" name = "submit" 
+            onclick = "submitForm(); return false;" 
+            class = "btn btn-default">
+            <?php
+                if ($id == -1)
+                    echo 'Создать турнир';
+                else
+                    echo 'Изменить турнир';
+            ?>
+        </button>
+    </div>    
 </form>
